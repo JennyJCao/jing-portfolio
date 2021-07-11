47b004cb86d500eb593a81ebfaa26ac35e131c18
JingCao <2410564208@qq.com>
2021-07-11 10:15:29 +0800

    first mommit

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/index.scss'
+
+
+const MyApp = ({Component, pageProps}) => {
+
+  return <Component {...pageProps} />
+}
+
+export default MyApp;
