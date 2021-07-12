2bced001e5f0039d26fad87e49d79fda6949adbd
JingCao <2410564208@qq.com>
2021-07-12 17:27:03 +0800

    fetch portfolios on client using axios and Graphql

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,26 +1,50 @@
 // import React from "react";
+import axios from "axios";
 
+// const apiCall = () => {
+//   return new Promise((res, rej) => {
+//     setTimeout(() => {
+//       res({testingData: 'just some testing data'});
+//     }, 200);
+//   })
+// }
 
-const apiCall = () => {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res({testingData: 'just some testing data'});
-    }, 200);
-  })
+const fetchPortfolios = () => {
+  // 如果多行可以用模板字符串 ``；一行用 ''
+  const query = `
+      query Portfolios {
+        portfolios {
+          _id,
+          title, 
+          company, 
+          companyWebsite, 
+          location, 
+          jobTitle description
+        }
+      }`;
+  // axios 的返回值都是res.data，所以res.data.data.portfolios才是要的portfolios数组
+  return axios.post('http://localhost:3000/graphql', {query: query})
+    .then(({data: {data: portfolios}}) => portfolios);
 }
 
 
-const Portfolios = (props) => {
+const Portfolios = ({portfolios}) => {
+
+
+
   return (
     <>
-      {props.testingData}
+      {/*{props.testingData}*/}
       <section className="section-title">
         <div className="px-2">
           <div className="pt-5 pb-4">
             <h1>Portfolios</h1>
           </div>
         </div>
+        {/*<button className="btn btn-primary" onClick={fetchPortfolios}>Fetch Data</button>*/}
       </section>
+      {/*测试是否接收到portfolios*/}
+      {/*{JSON.stringify(portfolios)}*/}
       <section className="pb-5">
         <div className="row">
           <div className="col-md-4">
@@ -65,15 +89,15 @@ const Portfolios = (props) => {
   )
 }
 
-
 Portfolios.getInitialProps = async () => {
-  const data = await apiCall();
-  return {...data};
+  const portfolios = await fetchPortfolios();
+  return {portfolios};
 }
 
-
-
-
+// Portfolios.getInitialProps = async () => {
+//   const data = await apiCall();
+//   return {...data};
+// }
 
 
 // class Portfolios extends React.Component {
