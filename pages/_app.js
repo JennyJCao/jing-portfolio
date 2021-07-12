d35955e557c112901fc230f78656952ea82c7fad
JingCao <2410564208@qq.com>
2021-07-12 11:29:59 +0800

    SSR Debugging

--- pages/_app.js
+++ pages/_app.js
@@ -7,21 +7,32 @@ import Hero from "@/components/shared/Hero";
 
 
 const MyApp = ({Component, pageProps}) => {
-
+  const isHomePage = () => Component.name === 'Home';
   return (
     <div className="portfolio-app">
       <Navbar/>
-      {pageProps.appData}
-      {Component.name === 'Home' && <Hero/>}
+      {isHomePage() && <Hero/>}
       <div className='container'>
         <Component {...pageProps} />
       </div>
+
+      {/* FOOTER STARTS */}
+      { isHomePage() &&
+          <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
+          <div className="container text-center">
+          <small>Copyright &copy; Your Website</small>
+          </div>
+          </footer>
+      }
+      {/* FOOTER ENDS */}
     </div>
   )
 }
 
 // getInitialProps 只在最上层调用
 // 如果app调用了，那么portfolios中的getInitialProps就不会调用了
+// 此时需要const initialProps = App.getInitialProps && await App.getInitialProps(context);
+// 用于执行当前组件（除了_app之外)的getInitialProps方法，获取参数。这里就是去执行portfolios的getInitialProps
 // MyApp.getInitialProps = async (context) => {
 //   const initialProps = App.getInitialProps && await App.getInitialProps(context);
 //   console.log(initialProps);
