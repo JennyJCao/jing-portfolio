de0f2cfc92d3c1f13fe45535b29ea70e26584acb
JingCao <2410564208@qq.com>
2021-07-14 13:02:58 +0800

    get portfolio with apollo

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,55 +1,18 @@
 import React from 'react';
-
-import {useRouter} from 'next/router';
-import axios from "axios";
-
+import {useQuery} from "@apollo/react-hooks";
+import {GET_PORTFOLIO} from "@/apollo/queries";
 
 
+const PortfolioDetail = ({query}) => {
 
-const fetchPortfolioById = (id) => {
-  // 如果多行可以用模板字符串 ``；一行用 ''
-  // id不要忘了加""
-  // 参数写在第二层，第一层的Portfolio只是给该操作起的一个名字，叫啥都行，后面如果跟参数就是variables
-  const query = `
-      query Portfolio {
-        portfolio(id: "${id}") {
-          _id,
-          title,
-          company,
-          companyWebsite,
-          location,
-          jobTitle,
-          description,
-          startDate,
-          endDate
-        }
-      }`;
-  // axios 的返回值都是res.data，所以res.data.data.portfolios才是要的portfolios数组
-  return axios.post('http://localhost:3000/graphql', {query: query})
-    .then(({data: {data: {portfolio: portfolio}}}) => portfolio);
+  // variables:  传参
+  const {loading, error, data } = useQuery(GET_PORTFOLIO, {variables: {id: query.id}});
 
-  // // 如果通过variables传参是一样的，这样写
-  // const query = `
-  //     query Portfolio($id: ID) {
-  //       portfolio(id: $id) {
-  //         _id,
-  //         title,
-  //         company,
-  //         companyWebsite,
-  //         location,
-  //         jobTitle,
-  //         description,
-  //         startDate,
-  //         endDate
-  //       }
-  //     }`;
-  // const variables = {id};
-  // return axios.post('http://localhost:3000/graphql', {query, variables})
-  //   .then(({data: {data: {portfolio: portfolio}}}) => portfolio);
+  if (loading) {
+    return 'Loading...';
+  }
 
-}
-
-const PortfolioDetail = ({portfolio}) => {
+  const portfolio = data && data.portfolio || {};
 
   // 这里的router.query.id要和文件名的 [id].js 保持一致，否则不生效
   // 例如： router.query.slug  --- [slug].js
@@ -95,40 +58,8 @@ const PortfolioDetail = ({portfolio}) => {
 }
 
 PortfolioDetail.getInitialProps = async ({query}) => {
-  const portfolio = await fetchPortfolioById(query.id);
-  return {portfolio};
+  return {query};
 }
 
-
-
-// const PortfolioDetail = () => {
-
-//   const router = useRouter();
-//   // const id = router.query.id;
-//   const {id} = router.query;
-
-//   // 这里的router.query.id要和文件名的 [id].js 保持一致，否则不生效
-//   // 例如： router.query.slug  --- [slug].js
-//   return (
-//     <h1>I am the detail page with ID: {id}</h1>
-//   )
-// }
-
-
-// class PortfolioDetail extends React.Component {
-
-//   // called on the server
-//   static getInitialProps({query}) {
-//     // what you return here will get into this.props
-//     return {query, test: 'jing'};
-//   }
-
-//   render() {
-//     const {id} = this.props.query;
-//     return (
-//       <h1>I am the detail page with ID: {id}, test is: {this.props.test}</h1>
-//     )
-//   }
-// }
 
 export default PortfolioDetail;
