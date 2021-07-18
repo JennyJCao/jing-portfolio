0983592506e4451690aa675d7234d80bf11b3e84
JingCao <2410564208@qq.com>
2021-07-18 15:53:02 +0800

    handle errors

--- pages/register.js
+++ pages/register.js
@@ -8,13 +8,18 @@ import withApollo from "@/hoc/withApollo";
 import Redirect from "@/components/shared/Redirect";
 
 const Register = () => {
+
+  const errorMessage = (error) => {
+    return (error.graphQLErrors && error.graphQLErrors[0].message) || 'something went wrong';
+  }
+
   return (
     <>
       <div className="bwm-form mt-5">
           <div className="row">
             <div className="col-md-5 mx-auto">
               <h1 className="page-title">Register</h1>
-              {/*第二种方式执行mutation，在类式组件中只能用这种方式；第一种是apollo/actions/index.js */}
+              {/*第二种方式执行mutation，在类式组件中只能用这种方式；第一种是apollo/actions/index.js*/}
               <Mutation mutation={SIGN_UP}>
                 { (signUpUser, {data, error}) =>
                   <>
@@ -23,6 +28,8 @@ const Register = () => {
                     }}/>
                     {/*注册成功后(data存在且data注册了），redirect */}
                     {data && data.signUp && <Redirect to="/login"/>}
+                    { error && <div className="alert alert-danger">{errorMessage(error)}</div>}
+
                   </>
                 }
               </Mutation>
