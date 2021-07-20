da89826a08422e3278498cd22c15f52a5404b2e3
JingCao <2410564208@qq.com>
2021-07-20 22:45:26 +0800

    prepare posts data

--- server/fakeDb/data.js
+++ server/fakeDb/data.js
@@ -1,11 +1,27 @@
 
 const mongoose = require('mongoose');
+const moment = require('moment');
+
+
 const user1Id = mongoose.Types.ObjectId();
 const user2Id = mongoose.Types.ObjectId();
+
 const forum1Id = mongoose.Types.ObjectId();
 const forum2Id = mongoose.Types.ObjectId();
 const forum3Id = mongoose.Types.ObjectId();
 const topic1Id = mongoose.Types.ObjectId();
+
+const post1Id = mongoose.Types.ObjectId();
+// 当前时间的七天前创建的post
+const post1CreatedAt = moment().subtract(7, 'days');
+const post2Id = mongoose.Types.ObjectId();
+const post2CreatedAt = moment(post1CreatedAt).add(1, 'days');
+const post3Id = mongoose.Types.ObjectId();
+const post3CreatedAt = moment(post2CreatedAt).add(1, 'days');
+const post4Id = mongoose.Types.ObjectId();
+const post4CreatedAt = moment(post3CreatedAt).add(1, 'days');
+
+
 
 const data = {
   users: [
@@ -106,6 +122,46 @@ const data = {
       content: "t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
       forumCategory: forum1Id,
       user: user1Id
+    }
+  ],
+  posts: [
+    {
+      _id: post1Id,
+      content: 'Hey there how are you ?',
+      slug: 'md43',
+      fullSlug: post1CreatedAt.toISOString() + ':md43',
+      topic: topic1Id,
+      user: user1Id,
+      createdAt: post1CreatedAt
+    },
+    {
+      _id: post2Id,
+      content: 'What do you think about this?',
+      slug: 'md59',
+      fullSlug: post2CreatedAt.toISOString() + ':md59',
+      topic: topic1Id,
+      user: user2Id,
+      createdAt: post2CreatedAt
+    },
+    {
+      _id: post3Id,
+      content: 'I think its nice (:',
+      slug: 'md59/md79',
+      fullSlug: post2CreatedAt.toISOString() + ':md59' + '/' + post3CreatedAt.toISOString() + ':md79',
+      topic: topic1Id,
+      user: user1Id,
+      parent: post2Id,
+      createdAt: post3CreatedAt
+    },
+    {
+      _id: post4Id,
+      content: 'Good to hear that!',
+      slug: 'md59/md79/md89',
+      fullSlug: post2CreatedAt.toISOString() + ':md59' + '/' + post3CreatedAt.toISOString() + ':md79' + '/' + post4CreatedAt.toISOString() + ':md89',
+      topic: topic1Id,
+      user: user2Id,
+      parent: post3Id,
+      createdAt: post4CreatedAt
     }
   ]
 }
