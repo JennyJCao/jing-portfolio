73bbb2a34ca61773becc4f8af38e782f15b3389e
JingCao <2410564208@qq.com>
2021-07-22 10:50:52 +0800

    display posts

--- server/graphql/models/Post.js
+++ server/graphql/models/Post.js
@@ -13,6 +13,7 @@ class Post {
     // 这里可以传入topic的id，也可以传入topic全部，mongoose will resolve it
     return this.Model
       .find({topic})
+      .sort('createdAt')
       .populate('topic')
       .populate('user')
       .populate({path: 'parent', populate: 'user'});// 填充parent中的user字段
