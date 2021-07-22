dfaf4f48980ea510b208e2a2835db23a599eb0d0
JingCao <2410564208@qq.com>
2021-07-23 00:04:30 +0800

    pord base url

--- next.config.js
+++ next.config.js
@@ -2,10 +2,15 @@
 
 const path = require('path');
 
+const dev = process.env.NODE_ENV !== 'production';
+
 module.exports = {
   webpack: config => {
     // 将项目路径映射为@，起了个别名，是个绝对路径
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
+  },
+  env: {
+    BASE_URL: dev ? 'http://localhost:3000/graphql' : 'https://jing-portfolio.herokuapp.com/graphql'
   }
 }
