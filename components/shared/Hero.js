e3d38053cd9a66e1bd131ec4776a86e9e66d1614
JingCao <2410564208@qq.com>
2021-07-23 16:51:27 +0800

    change hero name

--- components/shared/Hero.js
+++ components/shared/Hero.js
@@ -4,7 +4,7 @@ const Hero = () =>
   <section className="fj-hero">
     <div className="fj-hero-wrapper row">
       <div className="hero-left col-md-6">
-        <h1 className="white hero-title">Hey I'm Filip. Experienced full stack developer</h1>
+        <h1 className="white hero-title">Hey I'm Jing Cao. Experienced full stack developer</h1>
         <h2 className="white hero-subtitle">Check my portfolio and video tutorials</h2>
         <div className="button-container">
           <a href="" className="btn btn-main bg-blue ttu">See my work</a>
